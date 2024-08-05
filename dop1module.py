406d735e52d0c05eaacefa380d62132714bad9f8
Vlrri <likmih367@gmail.com>
2024-08-05 21:37:28 +0500

    Add files via upload

---
+++ dop1module.py
@@ -0,0 +1,6 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = sorted(students)
+GPA = sum(grades [0])/len(grades[0]), sum(grades [1])/len(grades[1]), sum(grades [2])/len(grades[2]), sum(grades [3])/len(grades[3]), sum(grades [4])/len(grades[4])
+klass = {students[0]: GPA[0], students[1]: GPA[1], students[2]: GPA[2], students[3]: GPA[3], students[4]: GPA[4]}
+print(klass)
